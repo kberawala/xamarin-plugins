e9f13d1a46220a9a5f159f5307a94d0e92b2ed45
Krunal Berawala <berawala.krunal@hotmail.com>
2018-05-30 06:29:08 +1000

    updated packages

--- Geofence/Geofence/Geofence.Plugin.Android/Properties/AssemblyInfo.cs
+++ Geofence/Geofence/Geofence.Plugin.Android/Properties/AssemblyInfo.cs
@@ -3,9 +3,8 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// General Information about an assembly is controlled through the following set of attributes.
+// Change these attribute values to modify the information associated with an assembly.
 [assembly: AssemblyTitle("Geofence.Plugin.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -18,13 +17,9 @@ using Android.App;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+// Major Version Minor Version Build Number Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// You can specify all the values or you can default the Build and Revision Numbers by using the '*'
+// as shown below: [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2018.5.29.149")]
+[assembly: AssemblyFileVersion("2018.5.29.149")]
